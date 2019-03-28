d1e0c1033a451f8be708b34772e352b8e94ce2f0
J.ason <754572491@qq.com>
2019-03-28 14:13:58 +0800

    Create github_auto_pull.js

---
+++ github_auto_pull.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const crypto = require('crypto');
+
+const secret = "Your webhook scret";
+const repo = "Your repo PATH (example : ~/xxx)";
+const exec = require('child_process').exec;
+
+http.createServer(function (req, res) {
+    req.on('data', function(chunk) {
+        let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+        if (req.headers['x-hub-signature'] == sig) {
+            console.log("start pull...")
+            exec('cd ' + repo + ' && git pull ', (error, stdout, stderr) => {
+            if (error) {
+              console.error(`执行出错: ${error}`);
+              return;
+            }
+        });
+    }});
+
+    res.end();
+}).listen(8080);
